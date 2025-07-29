9438a79ff433e7270858e1b327ca29b90508f511
fernandzxc <alifernandopinto@gmail.com>
2025-07-29 15:41:35 -0400

    actualizacion

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import "../assets/css/estilo.css"
+import "../assets/css/estilo.css";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
     <div className="bg-[url('./assets/img/home_desktop.jpg')] object-contain bg-cover bg-center h-screen pl-10 pt-10">
       <Header />
 
-      <div className="flex w-full justify-around h-4/5 items-center ">
+      <div className="flex w-full justify-around h-4/5 items-center">
         <div className="text-white flex flex-col gap-8 w-1/3">
           <h2 className="text-2xl">SO, YOU WANT TO TRAVEL TO</h2>
           <h1 className="text-9xl">SPACE</h1>
@@ -18,9 +19,11 @@ const Home = () => {
             world experience!
           </p>
         </div>
-        <button className="bg-white text-3xl w-52 h-52 rounded-full items-start">
-          <button className="bg-red-500 w-60 h-60 rounded-full  z-10 hover:bg-red-500">EXPLORE</button>
-        </button>
+        <div className="w-72 h-72 flex items-center justify-center">
+          <button className="button">
+            <Link to='/Destination' className="bg-white cursor-pointer text-3xl w-52 h-52 rounded-full align-text-center">EXPLORE</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
