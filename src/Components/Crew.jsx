123004a81857d9eea7dcdd769c0c7ed664d0d802
fernandzxc <alifernandopinto@gmail.com>
2025-08-19 09:55:13 -0400

    component  tecno

--- src/Components/Crew.jsx
+++ src/Components/Crew.jsx
@@ -1,39 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./Header";
 import douglas from "../assets/img/crew/image-douglas-hurley.png";
-import anousheh from "../assets/img/crew/image-anousheh-ansari.png";
-import mark from "../assets/img/crew/image-mark-shuttleworkth.png";
+import ansari from "../assets/img/crew/image-anousheh-ansari.png";
+import mark from "../assets/img/crew/image-mark-shuttleworth.png";
 import victor from "../assets/img/crew/image-victor-glover.png";
 
 const Crew = () => {
-  const [selectCrew, setSelectCrew] = useState('douglas')
+  const [selectCrew, setSelectCrew] = useState("douglas");
 
   const Crew = {
     douglas: {
       role: "COMMANDER",
       name: "DOUGLAS HURLEY",
       bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-      image: douglas
+      image: douglas,
     },
     mark: {
       role: "MISSION SPECIALIST",
       name: "MARK SHUTTLEWORTH",
       bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
-      image: mark
+      image: mark,
     },
     victor: {
       role: "PILOT",
       name: "VICTOR GLOVER",
       bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
-      image: victor
+      image: victor,
     },
-    douglas: {
+    ansari: {
       role: "FLIGHT ENGINNER",
-      name: "DOUGLAS HURLEY",
-      bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-      image: douglas
-    }
-  }
+      name: "ANOUSHEH ANSARI",
+      bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+      image: ansari,
+    },
+  };
+
+  const current = Crew[selectCrew];
 
   return (
     <div className='bg-[url("./assets/img/crew/crew_desktop.jpg")] object-contain bg-cover bg-center h-dvh pl-10 pt-10 overflow-hidden'>
@@ -43,22 +46,17 @@ const Crew = () => {
           <h1 className="text-2xl">
             <span className="text-gray-500">02 </span>MEET YOUR CREW
           </h1>
-          <h2 className="text-3xl text-gray-500">COMMANDER</h2>
-          <h3 className="text-5xl">DOUGLAS HURLEY</h3>
-          <p>
-            Douglas Gerald Hurley is an American engineer, former Marine Corps
-            pilot and former NASA astronaut. He launched into space for the
-            third time as commander of Crew Dragon Demo-2.
-          </p>
+          <h2 className="text-3xl text-gray-500">{current.role}</h2>
+          <h3 className="text-5xl">{current.name}</h3>
+          <p>{current.bio}</p>
           <div className="flex gap-5">
-            <button className="w-4 h-4 rounded-full bg-white"></button>
-            <button className="w-4 h-4 rounded-full bg-red-50/25"></button>
-            <button className="w-4 h-4 rounded-full bg-red-50/25"></button>
-            <button className="w-4 h-4 rounded-full bg-red-50/25"></button>
+            {Object.keys(Crew).map((key) => (
+              <button key={key} onClick={() => setSelectCrew(key)} className="w-4 h-4 rounded-full bg-white"></button>
+            ))}
           </div>
         </section>
         <figure className="flex justify-center w-full">
-          <img src={douglas} className="w-[45%]" />
+          <img src={current.image} className="w-[45%]" />
         </figure>
       </div>
     </div>
--- src/Components/Technology.jsx
+++ src/Components/Technology.jsx
@@ -6,7 +6,36 @@ const Technology = () => {
     <div className='bg-[url("./assets/img/technology/technology_desktop.jpg")] object-contain bg-cover bg-center h-dvh pl-10 pt-10 overflow-hidden'>
       <Header />
 
-      <div></div>
+      <div className="pt-10 px-14 text-white">
+        <h1 className="text-2xl pb-6">
+          <span className="text-gray-500">03 </span>SPACE LAUNCH 101
+        </h1>
+        <section className="flex">
+          <div className="flex flex-col text-3xl w-20 h-screen gap-14">
+            <button className="w-20 border-gray-500 border-2 rounded-full h-20 hover:bg-gray-500">
+              1
+            </button>
+            <button className="w-20 border-gray-500 border-2 rounded-full h-20 hover:bg-gray-500">
+              2
+            </button>
+            <button className="w-20 border-gray-500 border-2 rounded-full h-20 hover:bg-gray-500">
+              3
+            </button>
+          </div>
+
+          <div className="text-white bg-red-950">
+            <h2>THE TERMINOLOGY...</h2>
+            <h1 className="text-5xl text-amber-500">LAUNCH VEHICLE</h1>
+            <p>
+              A launch vehicle or carrier rocket is a rocket-propelled vehicle
+              used to carry a payload from Earth's surface to space, usually to
+              Earth orbit or beyond. Our WEB-X carrier rocket is the most
+              powerful in operation. Standing 150 metres tall, it's quite an
+              awe-inspiring sight on the launch pad!
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
