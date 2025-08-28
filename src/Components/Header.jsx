f54f5d844c40b7397961bd4594f5b3a6369b7957
fernandzxc <alifernandopinto@gmail.com>
2025-08-28 15:37:49 -0400

    responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         onClick={() => setIsMenuOpen(!isMenuOpen)}
         aria-label="Abrir menú de navegación"
       >
-        {isMenuOpen ? <FiX z-20 size={24} /> : <FiMenu size={24} />}
+        {isMenuOpen ? <FiX className="z-40" size={24} /> : <FiMenu className="z-40" size={24} />}
       </button>
 
       <div className="flex gap-16 w-3/4 py-1 justify-center text-white opacity-90 backdrop-blur-3xl max-lg:hidden">
@@ -73,7 +73,7 @@ const Header = () => {
       </div>
 
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-blue-950 backdrop-blur-3xl z-40 transform transition-transform duration-300 ease-in-out lg:hidden ${
+        className={`fixed top-0 right-0 h-full w-64 bg-blue-950/10 backdrop-blur-3xl z-40 transform transition-transform duration-300 ease-in-out lg:hidden ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
@@ -112,10 +112,9 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Overlay para cerrar el menú al hacer clic fuera */}
       {isMenuOpen && (
         <div
-          className="fixed inset-0 bg-black/20 z-30 lg:hidden"
+          className="fixed inset-0 bg-black/20 lg:hidden"
           onClick={() => setIsMenuOpen(false)}
         ></div>
       )}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,21 +5,21 @@ import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div className="bg-[url('./assets/img/home_desktop.jpg')] object-contain bg-cover bg-center h-screen pl-10 pt-10">
+    <div className="bg-[url('./assets/img/home/home_desktop.jpg')] object-contain bg-cover bg-center h-screen pl-10 pt-10 max-lg:bg-[url('./assets/img/home/home_tablet.jpg')] h-full">
       <Header />
 
-      <div className="flex w-full justify-around h-4/5 items-center">
-        <div className="text-white flex flex-col gap-8 w-1/3">
+      <div className="flex w-full justify-around h-4/5 items-center max-lg:flex-col">
+        <div className="text-white flex flex-col gap-8 w-1/3 max-lg:w-1/2 items-center py-10">
           <h2 className="text-2xl">SO, YOU WANT TO TRAVEL TO</h2>
           <h1 className="text-9xl">SPACE</h1>
-          <p>
+          <p className="max-lg:text-center">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
           </p>
         </div>
-        <div className="w-72 h-72 flex items-center justify-center">
+        <div className="w-72 h-72 flex items-center justify-center max-lg:w-48 h-48">
           <button className="button flex items-center justify-center">
             <Link
               to="/Destination"
